b01e3d10b6ed50d2d63885260c6550303315bf75
Jennifer <115043624+jenn0112@users.noreply.github.com>
2023-04-13 15:53:31 -0700

    Add files via upload

---
+++ students.sql
@@ -0,0 +1,17 @@
+create table students (
+id integer primary key,
+firstname varchar(40),
+lastname varchar(80),
+age NUMERIC
+);
+Insert into students (id, firstname, lastname, age)
+values (1, 'Fred', 'Flintstone', 35),
+(2, 'Wilma', 'Flintstone', 29),
+(3, 'Barney', 'Rubble', 33),
+(4, 'Betty', 'Rubble', 29),
+(5, 'Pebbles', 'Flintstone', 1),
+(6, 'Bam-Bam', 'Rubble', 1),
+(7, 'Jennifer', 'Oh', 21),
+(8, 'Joseph', 'Oh', 12),
+(9, 'Jenna', 'Oh', 18);
+
